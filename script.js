e18bdb96bc0d6f80bf9f952fd60eff1acb7952da
kanishkgits <kanishkcodes@gmail.com>
2025-08-02 11:24:20 +0530

    Added a function to load the array onto the page
    Added display function that loops through the myLibrary array and adds
    the books onto the main section

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ function addBookToLibrary(title, author, pages, read){
     const newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
 }
+
+const container = document.querySelector(".main");
+function display(){
+    for(let i = 0; i < myLibrary.length; i++){
+        const book = document.createElement("div");
+        book.innerHTML(`
+            <h2>${myLibrary[i].title}</h2>
+            <h3>${myLibrary[i].author}</h3>
+            <h3>No. of Pages: ${myLibrary[i].read}</h3>
+            <h3>ID: ${myLibrary[i].id}</h3>
+            `);
+        container.appendChild(book);
+    }
+}
